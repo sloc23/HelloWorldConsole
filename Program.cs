2a8ea5c454e873629d0893780455a89883a1b9c4
sloc23 <46392834+sloc23@users.noreply.github.com>
2019-01-07 12:59:34 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HelloWorldAPI;
+
+namespace HelloWorldConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMessageFacade oFacade = new HelloWorldFacade(new ConsoleHelloWorldMessage());
+            Console.WriteLine(oFacade.RenderMessage());
+            
+            oFacade = new HelloWorldFacade(new WebApplicationHelloWorldMessage());
+            Console.WriteLine(oFacade.RenderMessage());
+            
+            oFacade = new HelloWorldFacade(new WindowsServiceHelloWorldMessage());
+            Console.WriteLine(oFacade.RenderMessage());
+
+            Console.Read();
+        }
+    }
+}
